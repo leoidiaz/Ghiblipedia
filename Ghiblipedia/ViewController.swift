b39e99c58eb801bdad886145c667fb7b96850d0a
leoidiaz <zaid.leonardo@gmail.com>
2019-12-09 13:30:13 -0800

    Initial Commit

---
+++ Ghiblipedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ghiblipedia
+//
+//  Created by Leonardo Diaz on 12/9/19.
+//  Copyright © 2019 Leonardo Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
